c92c69fb398446a5eaa33a9c071bb9916d3ce085
Wulfram8 <psyh.ru.2014@gmail.com>
2018-05-02 20:59:57 +0300

    42.1.1

--- application.py
+++ application.py
@@ -33,7 +33,7 @@ def index():
         ksearch = '%' + search + '%'
         result = db.execute("SELECT * FROM books WHERE to_tsvector(author) || to_tsvector(title) @@ plainto_tsquery(:search) OR isbn LIKE :ksearch",
             {"search": search, "ksearch": ksearch})
-        return render_template("index.html", result = result, nickname = session["nickname"], login = session["login"])
+        return render_template("index.html", search=search, result = result, nickname = session["nickname"], login = session["login"])
 
 @app.route("/registration", methods=["GET", "POST"])
 def registr():
@@ -44,15 +44,15 @@ def registr():
         login = request.form.get("login")
         password = request.form.get("password")
 
-        if db.execute("SELECT * FROM users WHERE login = :login", {"login": login}).rowcount != 0:
+        if db.execute("SELECT * FROM users WHERE login = lower(:login)", {"login": login}).rowcount != 0:
             return "Login zanyat"
 
         if login and password:
             if not nickname:
-                db.execute("INSERT INTO users (login, password, nickname) VALUES (:login, :password, :nickname)",
+                db.execute("INSERT INTO users (login, password, nickname) VALUES (lower(:login), :password, :nickname)",
                     {"login": login, "password": password, "nickname": login})
             else:
-                db.execute("INSERT INTO users (login, password, nickname) VALUES (:login, :password, :nickname)",
+                db.execute("INSERT INTO users (login, password, nickname) VALUES (lower(:login), :password, :nickname)",
                     {"login": login, "password": password, "nickname": nickname})
             db.commit()
         else:
@@ -67,7 +67,7 @@ def signin():
     if request.method == "POST":
         login = request.form.get("login")
         password = request.form.get("password")
-        response = db.execute("SELECT * FROM users WHERE login = :login AND password = :password",
+        response = db.execute("SELECT * FROM users WHERE login = lower(:login) AND password = :password",
             {"login": login, "password": password}).first()
 
         if response:
@@ -89,8 +89,11 @@ def book(isbn):
     book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
     if book is None:
         return render_template("error.html", message="No book")
-    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "MdlurN92OnpSsJSFX19YJQ", "isbns": isbn}).json()
-    gr = {'average': "", 'rcount': ""}
-    gr['average'] = res['books'][0]['average_rating']
-    gr['rcount'] = res['books'][0]['work_ratings_count']
-    return render_template("book.html", gr=gr, book=book)
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "MdlurN92OnpSsJSFX19YJQ", "isbns": isbn})
+    if res.status_code == 200:
+        gr = {'average': "", 'rcount': ""}
+        gr['average'] = res.json()['books'][0]['average_rating']
+        gr['rcount'] = res.json()['books'][0]['work_ratings_count']
+        return render_template("book.html", gr=gr, book=book)
+    else:
+        return render_template("book.html", book=book)
