a7dfb63f6f374fc6b78adc9cf91a9d44cd6e091e
Wulfram8 <psyh.ru.2014@gmail.com>
2018-05-12 23:56:04 +0300

    lol

--- application.py
+++ application.py
@@ -25,7 +25,7 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        if session["login"] and session["nickname"]:
+        if session.get("login") and session.get("nickname"):
             return render_template("index.html", nickname = session["nickname"], login = session["login"])
         return render_template("index.html")
     if request.method == "POST":
@@ -71,7 +71,7 @@ def signin():
             {"login": login, "password": password}).first()
 
         if response:
-
+            session["id"] = response["id"]
             session["nickname"] = response["nickname"]
             session["login"] = response["login"]
             return redirect(url_for('index'))
@@ -80,20 +80,39 @@ def signin():
 
 @app.route("/exit")
 def exit():
+    session["id"] = ""
     session["nickname"] = ""
     session["login"] = ""
     return redirect(url_for('index'))
 
-@app.route("/book/<isbn>")
+@app.route("/book/<isbn>", methods=["GET", "POST"])
 def book(isbn):
-    book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
-    if book is None:
-        return render_template("error.html", message="No book")
-    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "MdlurN92OnpSsJSFX19YJQ", "isbns": isbn})
-    if res.status_code == 200:
-        gr = {'average': "", 'rcount': ""}
-        gr['average'] = res.json()['books'][0]['average_rating']
-        gr['rcount'] = res.json()['books'][0]['work_ratings_count']
-        return render_template("book.html", gr=gr, book=book)
-    else:
-        return render_template("book.html", book=book)
+    if request.method == "GET":
+        if not session["id"]:
+            return redirect(url_for('signin'))
+        book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
+        reviews = db.execute("SELECT text, nickname, rating FROM reviews JOIN users ON users.id=reviews.user_id WHERE reviews.book_isbn=:isbn",
+            {"isbn": isbn})
+        revUsers = db.execute("SELECT nickname FROM reviews JOIN users ON users.id=reviews.user_id").fetchall()
+        if session["nickname"] in revUsers:
+            kom = False
+        else:
+            kom = True
+        if book is None:
+            return render_template("error.html", message="No book")
+        res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "MdlurN92OnpSsJSFX19YJQ", "isbns": isbn})
+        if res.status_code == 200:
+            gr = {'average': "", 'rcount': ""}
+            gr['average'] = res.json()['books'][0]['average_rating']
+            gr['rcount'] = res.json()['books'][0]['work_ratings_count']
+            return render_template("book.html", kom=kom, reviews=reviews, gr=gr, book=book, nickname = session["nickname"], login = session["login"])
+        else:
+            return render_template("book.html", kom=kom, reviews=reviews, book=book, nickname = session["nickname"], login = session["login"])
+    if request.method == "POST":
+        if session["id"]:
+            review_text = request.form.get("review")
+            rating = request.form.get("rating")
+            db.execute("INSERT INTO reviews (text, user_id, book_isbn, rating) VALUES (:review, :user_id, :isbn, :rating)",
+                {"review": review_text, "user_id": session["id"], "isbn": isbn, "rating": rating})
+            db.commit()
+            return redirect(url_for('book', isbn=isbn))
